f3c86562ad5f1d48d07a3b8cf93c1e10a4052aa9
Zen <caiyuzhen00@gmail.com>
2023-01-08 18:48:16 +0800

    base:postCss learning

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,13 @@
 const postcssPresetEnv = require("postcss-preset-env");
+const postcssImport = require('postcss-import');
+const postcssMixin = require('postcss-mixins');
 
 module.exports = {
 	plugins: [
 		require("stylelint"),
 		require("autoprefixer"),
+		postcssMixin(),
+
 		postcssPresetEnv({
 			stage: 0, /* 使用 stage 0 阶段的 css 新特性 */
 		}),
